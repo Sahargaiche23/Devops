bcb6337c1c1cdba213ae94a18421d2a44f4283b5
Sahargaiche23 <sahar.gaiche@esprit.tn>
2025-03-29 23:29:27 +0100

    srccccccc

--- src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
@@ -1,62 +1,64 @@
 package tn.esprit.spring.kaddem.controllers;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.entities.Universite;
 import tn.esprit.spring.kaddem.services.IUniversiteService;
 
 import java.util.List;
+import java.util.Set;
 
-@Tag(name = "Gestion des universités")
 @RestController
+@AllArgsConstructor
 @RequestMapping("/universite")
-
 public class UniversiteRestController {
 	@Autowired
 	IUniversiteService universiteService;
-
 	// http://localhost:8089/Kaddem/universite/retrieve-all-universites
 	@GetMapping("/retrieve-all-universites")
-	@Operation(description = "récupérer la liste des universités")
-	@ResponseBody
 	public List<Universite> getUniversites() {
 		List<Universite> listUniversites = universiteService.retrieveAllUniversites();
 		return listUniversites;
 	}
-
 	// http://localhost:8089/Kaddem/universite/retrieve-universite/8
 	@GetMapping("/retrieve-universite/{universite-id}")
-	@Operation(description = "récupérer une université par son id")
-	@ResponseBody
 	public Universite retrieveUniversite(@PathVariable("universite-id") Integer universiteId) {
 		return universiteService.retrieveUniversite(universiteId);
 	}
 
 	// http://localhost:8089/Kaddem/universite/add-universite
 	@PostMapping("/add-universite")
-	@Operation(description = "ajouter une université")
-	@ResponseBody
 	public Universite addUniversite(@RequestBody Universite u) {
 		Universite universite = universiteService.addUniversite(u);
 		return universite;
 	}
 
-	// http://localhost:8089/Kaddem/universite/update-universite
-	@PutMapping("/update-universite")
-	@Operation(description = "modifier une université")
-	@ResponseBody
-	public Universite updateUniversite(@RequestBody Universite u) {
-		Universite universite= universiteService.updateUniversite(u);
-		return universite;
+	// http://localhost:8089/Kaddem/universite/remove-universite/1
+	@DeleteMapping("/remove-universite/{universite-id}")
+	public void removeUniversite(@PathVariable("universite-id") Integer universiteId) {
+		universiteService.deleteUniversite(universiteId);
 	}
 
-	// http://localhost:8089/Kaddem/universite/assignUniversiteToDepartement/1/1
-	@PutMapping("/assignUniversiteToDepartement/{universiteId}/{departementId}")
-	@Operation(description = "assigner une université à un département")
-	@ResponseBody
-	public void assignUniversiteToDepartement(@PathVariable("universiteId") Integer universiteId,@PathVariable("departementId") Integer departementId) {
-		universiteService.assignUniversiteToDepartement(universiteId,departementId);
+	// http://localhost:8089/Kaddem/universite/update-universite
+	@PutMapping("/update-universite")
+	public Universite updateUniversite(@RequestBody Universite u) {
+		Universite u1= universiteService.updateUniversite(u);
+		return u1;
 	}
+
+	//@PutMapping("/affecter-etudiant-departement")
+	@PutMapping(value="/affecter-universite-departement/{universiteId}/{departementId}")
+	public void affectertUniversiteToDepartement(@PathVariable("universiteId") Integer universiteId, @PathVariable("departementId")Integer departementId){
+		universiteService.assignUniversiteToDepartement(universiteId, departementId);
+	}
+
+	@GetMapping(value = "/listerDepartementsUniversite/{idUniversite}")
+	public Set<Departement> listerDepartementsUniversite(@PathVariable("idUniversite") Integer idUniversite) {
+
+		return universiteService.retrieveDepartementsByUniversite(idUniversite);
+	}
+
 }
+
