614cdfadb1070493f31318c6c9bc770eae805948
Sahargaiche23 <145070875+Sahargaiche23@users.noreply.github.com>
2025-04-05 00:35:31 +0200

    Update EtudiantRestController.java

--- src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/etudiant")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:8888")
 public class EtudiantRestController {
 
     private final IEtudiantService etudiantService;
