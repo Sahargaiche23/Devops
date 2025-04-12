ac69c84a27e1aba8522f1847adc42e24f17f2b2b
mariam53205 <mariam.benyohmes@esprit.tn>
2025-04-13 00:32:31 +0100

    add jenkinsefile

---
+++ src/main/java/tn/esprit/spring/kaddem/entities/Etudiant.java
@@ -0,0 +1,113 @@
+package tn.esprit.spring.kaddem.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Etudiant implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEtudiant;
+
+    private String nomE;
+    private String prenomE;
+
+    @Enumerated(EnumType.STRING)
+    private Option op;
+
+    @OneToMany(mappedBy = "etudiant", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Contrat> contrats;
+
+    @ManyToOne
+    @JsonIgnore
+    private Departement departement;
+
+    @ManyToMany(mappedBy = "etudiants")
+    @JsonIgnore
+    private List<Equipe> equipes;
+
+    // 🔹 Constructeurs
+    public Etudiant() {}
+
+    public Etudiant(String nomE, String prenomE) {
+        this.nomE = nomE;
+        this.prenomE = prenomE;
+    }
+
+    public Etudiant(String nomE, String prenomE, Option op) {
+        this.nomE = nomE;
+        this.prenomE = prenomE;
+        this.op = op;
+    }
+
+    public Etudiant(Integer idEtudiant, String nomE, String prenomE, Option op) {
+        this.idEtudiant = idEtudiant;
+        this.nomE = nomE;
+        this.prenomE = prenomE;
+        this.op = op;
+    }
+
+    // 🔹 Getters & Setters
+
+    public Integer getIdEtudiant() {
+        return idEtudiant;
+    }
+
+    public void setIdEtudiant(Integer idEtudiant) {
+        this.idEtudiant = idEtudiant;
+    }
+
+    public String getNomE() {
+        return nomE;
+    }
+
+    public void setNomE(String nomE) {
+        this.nomE = nomE;
+    }
+
+    public String getPrenomE() {
+        return prenomE;
+    }
+
+    public void setPrenomE(String prenomE) {
+        this.prenomE = prenomE;
+    }
+
+    public Option getOp() {
+        return op;
+    }
+
+    public void setOp(Option op) {
+        this.op = op;
+    }
+
+    public Set<Contrat> getContrats() {
+        return contrats;
+    }
+
+    public void setContrats(Set<Contrat> contrats) {
+        this.contrats = contrats;
+    }
+
+    public Departement getDepartement() {
+        return departement;
+    }
+
+    public void setDepartement(Departement departement) {
+        this.departement = departement;
+    }
+
+    public List<Equipe> getEquipes() {
+        return equipes;
+    }
+
+    public void setEquipes(List<Equipe> equipes) {
+        this.equipes = equipes;
+    }
+}
